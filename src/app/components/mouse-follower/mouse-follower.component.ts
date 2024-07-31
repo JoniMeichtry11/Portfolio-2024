cf413a3e1f1eec32654e8e1e71dc1983f6887b35
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-07-30 22:30:43 -0300

    feat: change colors in mouse foller

--- src/app/components/mouse-follower/mouse-follower.component.ts
+++ src/app/components/mouse-follower/mouse-follower.component.ts
@@ -6,16 +6,17 @@ import { Component, HostListener } from '@angular/core';
   standalone: true,
   imports: [NgStyle],
   templateUrl: './mouse-follower.component.html',
-  styleUrl: './mouse-follower.component.scss'
+  styleUrl: './mouse-follower.component.scss',
 })
 export class MouseFollowerComponent {
-  gradientStyle: string = 'radial-gradient(600px at 50% 50%, rgba(29, 78, 216, 0.15), transparent 80%)';
+  gradientStyle: string =
+    'radial-gradient(600px at 50% 50%, rgba(255, 100, 100, 0.15), transparent 80%)';
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
     const { pageX, pageY } = event;
     const offsetX = pageX;
     const offsetY = pageY;
-    this.gradientStyle = `radial-gradient(600px at ${offsetX}px ${offsetY}px, rgba(29, 78, 216, 0.15), transparent 80%)`;
+    this.gradientStyle = `radial-gradient(600px at ${offsetX}px ${offsetY}px, rgba(255, 100, 100, 0.15), transparent 80%)`;
   }
 }
